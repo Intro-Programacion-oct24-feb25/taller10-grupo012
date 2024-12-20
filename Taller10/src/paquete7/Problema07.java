82fa5a504b612d1f4e9cbc6233b06113e300aeda
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-12-20 11:05:17 -0500

    Problemas resueltos

--- Taller10/src/paquete7/Problema07.java
+++ Taller10/src/paquete7/Problema07.java
@@ -9,5 +9,28 @@ package paquete7;
  * @author Usuario iTC
  */
 public class Problema07 {
-    
+
+    public static void main(String[] args) {
+
+        double[][] dato1 = {{0, 2, 0}, {1, 0, 0}};
+        // double [][] dato1 = {{0,0,0}, {0,0,0}};
+        double sumatoria = 0;
+        int fila;
+        int columna;
+
+        for (fila = 0; fila < dato1.length; fila++) {
+            for (columna = 0; columna < dato1[fila].length; columna++) {
+
+                sumatoria = sumatoria + dato1[fila][columna];
+            }
+        }
+        
+        if (sumatoria == 0) {
+            System.out.printf("\nEl arreglo es NULO(La suma de "
+                    + "sus valores es %.2f)\n", sumatoria);
+        } else {
+            System.out.printf("\nEl arreglo NO es NULO (La suma de sus "
+                    + "valores es %.2f)\n", sumatoria);
+        }
+    }
 }
--- Taller10/src/paquete8/Problema08.java
+++ Taller10/src/paquete8/Problema08.java
@@ -9,5 +9,48 @@ package paquete8;
  * @author Usuario iTC
  */
 public class Problema08 {
-    
+
+    public static void main(String[] args) {
+
+        double[][] dato1 = {{1, 2, 3}, {6, 8, 9}};
+        double[][] dato2 = {{10, 1, 2}, {10, 9, 1}};
+        // En función de los arreglos dados, genere
+        // un arreglo que tenga los siguientes valores
+        /*
+         *  1    1    2
+         *  6    8    1
+         */
+        double[][] dato3 = new double[2][3];
+        int fila;
+        int columna;
+        
+        for (fila = 0; fila < dato3.length; fila++) {
+            for (columna = 0; columna < dato3[fila].length; columna++) {
+                if (columna == 0 || (fila == 1 && columna == 1)) {
+                    dato3[fila][columna] = dato1[fila][columna];
+                } else {
+                    dato3[fila][columna] = dato2[fila][columna];
+                }
+            }
+        }
+
+        System.out.println("Resultado:");
+
+        for (fila = 0; fila < dato3.length; fila++) {
+            for (columna = 0; columna < dato3[fila].length; columna++) {
+                System.out.printf("%.0f\t", dato3[fila][columna]);
+            }
+            System.out.println();
+        }
+
+        for (fila = 0; fila < dato3.length; fila++) {
+            for (columna = 0; columna < dato3[fila].length; columna++) {
+                System.out.printf("La posicion[%d][%d] tiene un "
+                        + "valor de: %.0f%n",
+                        fila,
+                        columna,
+                        dato3[fila][columna]);
+            }
+        }
+    }
 }
